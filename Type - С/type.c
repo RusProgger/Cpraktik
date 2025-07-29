03de1632eb7d484fa690ada4d530da80aa6acf14
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-07-29 15:56:55 +0300

    Add files via upload

---
+++ Type - С/type.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a = 10;
+    int b = 15;
+
+    unsigned int num = 100;
+    double num_2 = 19.45335;
+
+    float x2 = 2.67f;
+
+    printf("%d\n", a);
+    printf("%.5f\n", num_2);
+
+
+
+
+    printf("Hello, world!");
+
+    return 0;
+}
