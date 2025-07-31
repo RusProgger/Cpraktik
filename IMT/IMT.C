7438be329ece5ca30f8c6fe5cb2f31a2593144cb
RusProgger <ruslantimka92@gmail.com>
2025-07-31 18:50:42 +0300

    Индекс массы тела, добавлены условия вывода

--- IMT/IMT.C
+++ IMT/IMT.C
@@ -30,8 +30,27 @@ int main() {
     // результат
 
     float res_imt = mass / (height * height);
-
     
 
+    if(res_imt < 18.5) {
+        printf("Недостаток веса. Ваш индекс массы: %.3f", res_imt);
+    }
+
+    else if(res_imt >= 18.5 && res_imt <= 24.9 ) {
+        printf("Нормальный вес: %.3f", res_imt);
+    }
+
+    else if(res_imt >= 25 && res_imt <= 29.9 ) {
+        printf("Избыточный вес: %.3f", res_imt);
+    }
+
+    else if(res_imt >= 30 ) {
+        printf("Ожирение: %.3f", res_imt);
+    }
+    else {
+        printf("Ошибка, данные не существуют.");
+    }
+
+    
     return 0;
 }
