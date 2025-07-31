78aef459623cb0cde3a9c9839418c2250ff5f403
RusProgger <ruslantimka92@gmail.com>
2025-07-31 19:15:39 +0300

    Добавлен цикл

--- IMT/IMT.C
+++ IMT/IMT.C
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 
 void TextmessageHeight() {
     printf("Enter height: ");
@@ -8,49 +9,62 @@ void TextmessageMass() {
     printf("Enter mass: ");
 }
 
+// void MenuExit() {
+//     printf("Enter exit: ");
+// }
+
 
 int main() {
 
+    char *locale = setlocale(LC_ALL, "");
     // переменные для массы-тела, и роста
 
     float height, mass;
+    // int exit;
 
-    // вызов функции рост
-
-    TextmessageHeight();
-
-    scanf("%f", &height);
-
-    // вызов функции кг
-
-    TextmessageMass();
-
-    scanf("%f", &mass);
-
-    // результат
-
-    float res_imt = mass / (height * height);
+    // Menu 
+    // MenuExit();
+    // scanf("%d", &exit);
     
 
-    if(res_imt < 18.5) {
-        printf("Недостаток веса. Ваш индекс массы: %.3f", res_imt);
-    }
+    // цикл 
+    do {
+        // вызов функции рост
+        
+        TextmessageHeight();
 
-    else if(res_imt >= 18.5 && res_imt <= 24.9 ) {
-        printf("Нормальный вес: %.3f", res_imt);
-    }
+        scanf("%f", &height);
 
-    else if(res_imt >= 25 && res_imt <= 29.9 ) {
-        printf("Избыточный вес: %.3f", res_imt);
-    }
+        // вызов функции кг
 
-    else if(res_imt >= 30 ) {
-        printf("Ожирение: %.3f", res_imt);
-    }
-    else {
-        printf("Ошибка, данные не существуют.");
-    }
+        TextmessageMass();
 
-    
+        scanf("%f", &mass);
+
+        // результат
+
+        float res_imt = mass / (height * height);
+        
+
+        if(res_imt < 18.5) {
+            printf("Недостаток веса. Ваш индекс массы: %.3f\n", res_imt);
+        }
+
+        else if(res_imt >= 18.5 && res_imt <= 24.9 ) {
+            printf("Нормальный вес: %.3f\n", res_imt);
+        }
+
+        else if(res_imt >= 25 && res_imt <= 29.9 ) {
+            printf("Избыточный вес: %.3f\n", res_imt);
+        }
+
+        else if(res_imt >= 30 ) {
+            printf("Ожирение: %.3f\n", res_imt);
+        }
+
+    } while(true);
+
+    getchar();
+    getchar();
     return 0;
 }
