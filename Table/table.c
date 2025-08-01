a504e8be24b88fe6f955a492ea77c8a81ef25353
RusProgger <ruslantimka92@gmail.com>
2025-08-01 14:14:23 +0300

    Задача, сколько сможем сделать столов

--- Table/table.c
+++ Table/table.c
@@ -5,8 +5,11 @@ int main() {
     int tableN;
     int tableMax = 4;
 
-    
+    scanf("%d", &tableN);
 
+    int titleTable = (int)tableN / (int)tableMax;
+
+    printf("%d", titleTable);
 
     return 0;
 }
