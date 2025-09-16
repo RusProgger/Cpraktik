3623c48b52486e48922a7491a110bfd94211b35a
RusProgger <ruslantimka92@gmail.com>
2025-09-16 13:26:41 +0300

    Вывод времени

---
+++ time/time.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int year;            // год
+    int month;           // месяц
+    int day;             // день
+    int hours;           // часы
+    int minutes;         // минуты
+    int seconds;         // секунды
+    int utc_offset;      // смещение в часах относительно UTC
+
+    year = 2025;
+    month = 7;
+    day = 16;
+    hours = 13;
+    minutes = 20;
+    seconds = 45;
+    utc_offset = 3;
+
+    // Вывод в нужном формате
+    printf("%04d-%02d-%02dT%02d:%02d:%02d%+03d\n",
+           year, month, day, hours, minutes, seconds, utc_offset);
+
+    return 0;
+}
