ac13bcbc0b46fb7258be7257d5c1e4c5c0b4862c
RusProgger <ruslantimka92@gmail.com>
2025-10-27 15:45:17 +0200

    Решение 4 задачи Преобразование из `double` в `float` на языке Си

---
+++ Type_Casting/4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+
+    ### 4. Преобразование из `double` в `float`
+    Напишите программу, которая принимает значение типа `double`, преобразует его в `float` и выводит результат с округлением до 2 знаков после запятой.
+
+*/
+
+
+int main() {
+
+    double number;
+    printf("Enter a number (double): ");
+    scanf("%lf", &number);
+
+     // Преобразование в float
+    float float_number = (float)number;
+
+    // вывод 
+    printf("Enter float output:  %.2f", float_number);
+
+    getchar();
+    return 0;
+}
