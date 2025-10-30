37315eedf8d3c0f550583980dbb214c00dee3e1d
RusProgger <ruslantimka92@gmail.com>
2025-10-30 21:49:09 +0200

    Работа с типом char в языке Си

--- variable/char.c
+++ variable/char.c
@@ -13,5 +13,17 @@ int main() {
     char letters[] = "B";
     printf("String: %s\n", letters);
 
+    // неизменяемая массив символов
+
+    char* name = "Denis";
+
+    printf("Name user: %s\n", name); // вывод Denis
+
+    // попробудем изменить символ. 
+
+    // name[2] = 'm'; // ошибка попытка изменить литерал строки, приводит к ошибке
+
+    // printf("Name user: %s\n", name);
+
     return 0;
 }
