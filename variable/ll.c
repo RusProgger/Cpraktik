464334d4a6e99385e88df62d12f8c355d9b19e5b
RusProgger <ruslantimka92@gmail.com>
2025-10-30 23:52:55 +0200

    Работа с long long int

--- variable/ll.c
+++ variable/ll.c
@@ -10,8 +10,11 @@ int main() {
 
     long int numB = 642910056L;
 
+    char repo1 = 65;
+
     printf("Long long int value: %lld\n", bigNumber);
     printf("Long int value: %ld\n", numB);
+    printf("Char value: %ld\n", repo1);
 
     return 0;
 }
