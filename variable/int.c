978ce6f908fd2e8d9cfc4289f13377f9c1f2a8fe
RusProgger <ruslantimka92@gmail.com>
2025-10-30 20:39:54 +0200

    Работа с типом int в си

--- variable/int.c
+++ variable/int.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// тип int занимает 4 байта
+
 int main() {
 
     for(int i = 0; i < 50; i++) {
@@ -16,11 +18,17 @@ int main() {
 
     int numberA = 1000;
 
-    printf("\n%d", numberA);
+    printf("\n%d\n\n", numberA);
 
 
     // добавить ввод от пользователя с помощью scanf, чтобы программа не просто выводила числа, а принимала их и считала что-то (например, сумму или произведение).
 
+    int numberUser;
+    printf("Input number: ");
+
+    scanf("%d", &numberUser);
+
+    printf("Number - %d\n", numberUser);
 
     return 0;
 }
