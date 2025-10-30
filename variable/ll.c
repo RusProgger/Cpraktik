72aa4b56a133f072375f7b5f4fc32416042517f7
RusProgger <ruslantimka92@gmail.com>
2025-10-30 23:49:08 +0200

    Работа с long long int

--- variable/ll.c
+++ variable/ll.c
@@ -4,14 +4,14 @@ int main() {
 
     // long long int 
 
-    long long int bigNumber = 66435676576463437L;
+    long long int bigNumber = 66435676576463437LL;
 
     // long int
 
     long int numB = 642910056L;
 
-    printf("%lld\n", bigNumber);
-    printf("%ld\n", numB);
+    printf("Long long int value: %lld\n", bigNumber);
+    printf("Long int value: %ld\n", numB);
 
     return 0;
 }
