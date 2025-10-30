95e49b922c3d0942b5a4dc96d2320e53c6da0c12
RusProgger <ruslantimka92@gmail.com>
2025-10-30 21:16:54 +0200

    Работа с типом float в языке Си

--- variable/float.c
+++ variable/float.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
+
+    double fn = 4.7744457;
+    printf("%.7f\n", fn); // все ок результат точный.
+
+
+    float fn1 = 4.7744457;
+    printf("%.7f\n", fn1); // float округляет только до 7 цифр. 
+
+
     
+
+
     return 0;
 }
