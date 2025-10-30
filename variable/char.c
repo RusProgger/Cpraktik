721684a0663fed7a0be582a93fdd58c4be76ac74
RusProgger <ruslantimka92@gmail.com>
2025-10-30 21:39:13 +0200

    Работа с типом char в языке Си

--- variable/char.c
+++ variable/char.c
@@ -2,7 +2,16 @@
 
 int main() {
 
-    
+    // символ
+    char letter = 'A';
+
+    // вывод символа
+
+    printf("Symbol: %c\n", letter);
+
+    // массив символов
+    char letters[] = "B";
+    printf("String: %s\n", letters);
 
     return 0;
 }
