2ff0586128c85923232cc733404b564de3fa1fcb
RusProgger <ruslantimka92@gmail.com>
2025-11-14 17:32:54 +0200

    Многомерные массивы в Си

--- multiArray/multiarray.c
+++ multiArray/multiarray.c
@@ -4,7 +4,7 @@ int main() {
 
     // Многомерный массив 1- строки, 2 столбцы
 
-    int arrs[2][2] = {
+    char arrs[2][2] = {
 
         {'A', 'B'}, 
         {'C', 'D'}
@@ -13,7 +13,17 @@ int main() {
     printf("Element array[0][0] = %c\n", arrs[0][0]);
     printf("Element array[0][1] = %c\n", arrs[0][1]);
     printf("Element array[1][0] = %c\n", arrs[1][0]);
-    printf("Element array[1][0] = %c\n", arrs[1][1]);
+    printf("Element array[1][1] = %c\n", arrs[1][1]);
+
+    // создадим цикл для прохода по массиву
+    
+    for(int i = 0; i < sizeof(arrs) / sizeof(arrs[0]); i++) {         
+        for(int j = 0; j < sizeof(arrs[0]) / sizeof(arrs[0][0]); j++) { 
+            printf("%c ", arrs[i][j]);
+    }
+        printf("\n");
+}
+
 
 
     getchar();
