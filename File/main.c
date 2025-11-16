7f18bf677f1486b73bcdd9d2d6ca0db6060aab41
RusProgger <ruslantimka92@gmail.com>
2025-11-17 00:23:41 +0200

    Работа с файлами в языке Си

--- File/main.c
+++ File/main.c
@@ -4,9 +4,12 @@ int main() {
 
     // file
 
+    char* name = "Hello, World!!";
     FILE * files;
     files = fopen("file.txt", "w");
-
+    if(files != NULL) {
+        fputs(name, files);
+    }
 
     // Закрытие файла
 
