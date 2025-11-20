a6da27eb3acfe1ddfecf8245e51ba44510f85ecf
RusProgger <ruslantimka92@gmail.com>
2025-11-20 03:23:15 +0200

    Работа с файлами в Си

--- File/files.c
+++ File/files.c
@@ -4,7 +4,15 @@ int main() {
 
     // Создаем константу с именем number
     const int number;
-    
+    fgets(number, sizeof(number), stdin);
+
+    FILE * f = fopen("fileNumber.txt", "a");
+
+    if(f != NULL) {
+        fputs(number, f);
+
+        
+    }
 
 
     return 0;
