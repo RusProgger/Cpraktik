1efcb059c2ec2e4f377993bfbfbf4ca8aa1d6f5c
RusProgger <ruslantimka92@gmail.com>
2025-11-30 13:53:27 +0200

    Работа с функциями в Си, добавлен указатели

--- Функции/functionPointer.c
+++ Функции/functionPointer.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 void messageNumber(int a, int b, int c, int* outA, int* outB, int* outC) {
-    
+    *outA = a;
+    *outB = b;
+    *outC = c;
 }
 
+
+
 int main() {
+
 
 
 
