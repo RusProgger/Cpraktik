7c66b5d550b03e4e8413daa75ef8616b62cf88da
RusProgger <ruslantimka92@gmail.com>
2025-12-04 01:55:27 +0200

    Работа с bool в Си

--- Bool/bool.c
+++ Bool/bool.c
@@ -8,11 +8,11 @@ int main() {
     // По умолчанию в Си нет встроенного типа bool, как в других языках. 
     // Поэтому тут нужно подключать библиотеку stdbool.h
 
-    // код который будет работать толкьо с библиотекой stdboolюр
+    // код который будет работать толкьо с библиотекой stdbool.h
 
     // Проверим пользователь женат?
 
-    bool isMarried = false; // false, пользователь не женат.
+    bool isMarried = true; // false, пользователь не женат.
 
     if(isMarried == true) {
         puts("User married true");
@@ -20,7 +20,7 @@ int main() {
         puts("User married false");
     }
 
-    printf("%s\n", (10 > 2) ? "true" : "false");
+    printf("10 > 2: %s\n", (10 > 2) ? "true" : "false");
 
     printf("----------------\n");
     return 0;
