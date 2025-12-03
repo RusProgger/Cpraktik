4f34d48dc2f506f4ecaa3d65c7edd85790aaeced
RusProgger <ruslantimka92@gmail.com>
2025-12-03 14:14:41 +0200

    Создание структуры

--- Структуры/struct.c
+++ Структуры/struct.c
@@ -14,9 +14,13 @@ int main() {
 
     // создание переменной
 
-    
+    struct Person p1 = {"Ruslan", 30};
+    struct Person* pt = &p1;
 
+    // вывод структуры
 
+    printf("Name: %s\n", p1.name);
+    printf("Age: %d\n", p1.age);
 
     return 0;
 }
