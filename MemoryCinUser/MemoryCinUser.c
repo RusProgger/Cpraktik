3426d2ca443d6892a28e700990f33ad357ff60c9
RusProgger <ruslantimka92@gmail.com>
2025-12-06 15:33:37 +0200

    Memory cin user

--- MemoryCinUser/MemoryCinUser.c
+++ MemoryCinUser/MemoryCinUser.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// константа для максимального значения выделения памяти в байтах
+#define MAX_SIZE_USER 2500
+
 int main() {
 
-    
+
 
     return 0;
 }
