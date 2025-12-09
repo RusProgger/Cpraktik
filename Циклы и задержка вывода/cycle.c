28d294d88fd7ff0f0a64b2962f689a944f0d8c48
RusProgger <ruslantimka92@gmail.com>
2025-12-09 02:32:11 +0200

    Циклы и задержка вывода

--- Циклы и задержка вывода/cycle.c
+++ Циклы и задержка вывода/cycle.c
@@ -3,11 +3,10 @@
 
 int main() {
 
-    int time = 1;
-
     for(int i = 1; i <= 5; i++) {
         Sleep(1000);
         printf("%d\n", i);
+        fflush(stdout); // Очистка буфера 
     }
 
     puts("cycle exit...");
