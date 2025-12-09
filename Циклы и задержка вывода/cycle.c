4230543a4014836bf7395143b831f20a072a1af7
RusProgger <ruslantimka92@gmail.com>
2025-12-09 02:29:01 +0200

    Циклы и задержка вывода

--- Циклы и задержка вывода/cycle.c
+++ Циклы и задержка вывода/cycle.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <Windows.h>
 
 int main() {
 
+    int time = 1;
+
+    for(int i = 1; i <= 5; i++) {
+        Sleep(1);
+        printf("%d\n", i);
+    }
+
+    puts("cycle exit...");
+
+    getchar();
     return 0;
 }
