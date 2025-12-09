737ffd11fe88a708b1e1b015589609f09805d5b1
RusProgger <ruslantimka92@gmail.com>
2025-12-09 03:08:06 +0200

    Добавлена практика "Практика решение первой задачи"

---
+++ Циклы и задержка вывода/Task/1/task1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+
+    for(int i = 1; i <= 10; i++) {
+
+        printf("\r%d", i);
+        fflush(stdout);
+        Sleep(1000);
+    }
+
+    return 0;
+}
