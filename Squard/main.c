2dd2926cbbf97ea046d6c1a7f51c92df181e5d98
RusProgger <ruslantimka92@gmail.com>
2026-02-05 01:39:13 +0200

    Написать программу которая считывает размер стороны квадрата и затем выводит его с помощью звездочек

--- Squard/main.c
+++ Squard/main.c
@@ -10,7 +10,11 @@ int main() {
     scanf_s("%d", &size);
 
     for(i = 0; i < size; i++) {
-        //
+        // Вложеный цикл 
+
+        for (j = 0; j < size; j++) {
+            printf("*");
+        }
     }
 
     getchar();
