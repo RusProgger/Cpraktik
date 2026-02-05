2bcd09112f88804c1637039c056ebdb2486a8534
RusProgger <ruslantimka92@gmail.com>
2026-02-05 02:20:31 +0200

    Факториал числа

--- Factorial/factorial.c
+++ Factorial/factorial.c
@@ -2,7 +2,20 @@
 
 #include <stdio.h>
 
+
+// Реализовать функцию 
+int Fact() {
+
+    
+}
+
 int main() {
+
+    int factor_number;
+
+    // Ввод пользователя
+    printf("Enter number: ");
+    scanf_s("%d", &factor_number);
 
 
     getchar();
