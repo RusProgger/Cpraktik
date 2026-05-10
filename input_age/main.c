134b9d919b369eae8795c5915a5d3f6b7fa071c4
RusProgger <ruslantimka92@gmail.com>
2026-05-10 22:44:49 +0300

    Исправление реализация проверки условий на ввод числа.

--- input_age/main.c
+++ input_age/main.c
@@ -2,14 +2,18 @@
 
 int main() {
 
-    unsigned int age = 0;
+    unsigned int age;
 
     printf("Enter age: ");
     scanf_s("%d", &age);
 
     // Реализация условий
 
-    
+    if(age > 0 && age <= 14) {
+        printf("Child: %d", age);
+    } else if(age >= 15 && age <= 18) {
+        printf("Teenager: %d", age);
+    }
 
 
 
