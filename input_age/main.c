bf15f745444fb607c3616e1b6f654c83e5fc70e7
RusProgger <ruslantimka92@gmail.com>
2026-05-10 22:52:35 +0300

    Редактирование проекта, исправлены ошибки

--- input_age/main.c
+++ input_age/main.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
 int main() {
-
     unsigned int age;
 
     printf("Enter age: ");
-    scanf_s("%d", &age);
+    scanf_s("%u", &age);
 
-    // Реализация условий
-
-    if(age >= 0 && age <= 14) {
-        printf("Child: %d", age);
-    } else if(age >= 15 && age <= 18) {
-        printf("Teenager: %d", age);
-    } else if(age >= 19 && age <= 45) {
-        printf("Young: %d", age);
-    } else if (age >= 46 && age <= 62) {
-        printf("Elderly: %d", age);
-    } else if (age >= 63 && age <= 100) {
-        printf("Old: %d", age);
-    } else {
-        printf("There is no such age.");
+    if (age <= 14) {
+        printf("Child: %u", age);
+    } 
+    else if (age <= 18) {
+        printf("Teenager: %u", age);
+    } 
+    else if (age <= 45) {
+        printf("Young: %u", age);
+    } 
+    else if (age <= 62) {
+        printf("Elderly: %u", age);
+    } 
+    else if (age <= 100) {
+        printf("Old: %u", age);
+    } 
+    else {
+        printf("Invalid age.");
     }
 
-    getchar();
     return 0;
 }
