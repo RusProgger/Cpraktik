0e97ac94aac1564d69d38a39125dddd1078aa149
RusProgger <ruslantimka92@gmail.com>
2026-06-10 15:43:04 +0300

    Add file memory_volume_type/type.c

---
+++ memory_volume_type/type.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("MemoryVolume char: %d\n", sizeof(char));
+    printf("MemoryVolume short: %d\n", sizeof(short));
+    printf("MemoryVolume float: %d\n", sizeof(float));
+    printf("MemoryVolume int: %d\n", sizeof(int));
+    printf("MemoryVolume double: %d\n", sizeof(double));
+
+    printf("\n-----------------------\n");
+
+    printf("MemoryVolume char: %zu\n", sizeof(char));
+    printf("MemoryVolume short: %zu\n", sizeof(short));
+    printf("MemoryVolume int: %zu\n", sizeof(int));
+    printf("MemoryVolume double: %zu\n", sizeof(double));
+    printf("MemoryVolume float: %zu\n", sizeof(float));
+    printf("MemoryVolume long: %zu\n", sizeof(long));
+    printf("MemoryVolume long long: %zu\n", sizeof(long long));
+    printf("MemoryVolume pointer: %zu\n", sizeof(void *));
+
+    getchar();
+    return 0;
+}
