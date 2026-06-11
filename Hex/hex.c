41e7225bb3f8f5893f887e193925685b4addcb11
RusProgger <ruslantimka92@gmail.com>
2026-06-11 19:20:40 +0300

    Add file hex.c

--- Hex/hex.c
+++ Hex/hex.c
@@ -9,7 +9,7 @@ int main() {
     // проверка если число меньше 0, то выводим сообщение о ошибке: 
 
     if(number < 0) {
-        printf("Error. Negative number.")
+        printf("Error. Negative number.");
         return 1;
     }
 
