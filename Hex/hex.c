872a572551c1d22aa2f1eb0685c648c557995077
RusProgger <ruslantimka92@gmail.com>
2026-06-11 19:28:30 +0300

    Update file add clear buffer

--- Hex/hex.c
+++ Hex/hex.c
@@ -3,8 +3,13 @@
 int main() {
 
     int number = 0;
+    int symbol;
+
     printf("Enter number: ");
     scanf_s("%d", &number);
+
+    while ((symbol = getchar()) != '\n' && symbol != EOF);
+
 
     // проверка если число меньше 0, то выводим сообщение о ошибке: 
 
@@ -15,7 +20,7 @@ int main() {
 
     printf("Number: %#08x\n", number);
 
-    
+
 
     // pause 
 
