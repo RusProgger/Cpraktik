def4e7acb6e45112cb33ce72ba257af7419c8370
RusProgger <ruslantimka92@gmail.com>
2026-08-03 14:46:19 +0300

    Add task progress

--- progress/progress.c
+++ progress/progress.c
@@ -1,11 +1,14 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 // Используем таблицу символов https://www.industrialnets.ru/files/misc/ascii.pdf
 
 
 int main() {
 
-    
-
+    for(int i = 0; i < 30; i++) {
+        putchar(176);
+        // printf("%c", 176);
+    }
+ 
     return 0;
 }
