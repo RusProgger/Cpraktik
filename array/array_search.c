56c7a153e287bfe760f37353c0cb3f091e5c042b
RusProgger <ruslantimka92@gmail.com>
2026-08-05 22:22:11 +0300

    Add task array search

--- array/array_search.c
+++ array/array_search.c
@@ -5,7 +5,26 @@
 
 int main() {
 
+    int arr[] = {5, 10, 15, 20, 25};
 
+    int count = sizeof(arr) / sizeof(arr[0]);
+
+    int index = 0; // для счетчика
+
+    for(int i = 0; i < count; i++) {
+
+        // Проверяем есть ли число 20 в массиве
+        if(arr[i] == 20) {
+            index = 1;
+            break;
+        }
+    }
+
+    if(index) {
+        printf("Found!\n");
+    } else {
+        printf("Not found!");
+    }
 
     return 0;
 }
