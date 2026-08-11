6a3f5d650b0026565c7261072c60fd340905865b
RusProgger <ruslantimka92@gmail.com>
2026-08-11 15:19:34 +0300

    AND - bitwise operation

--- Bitwise_operation/operation.c
+++ Bitwise_operation/operation.c
@@ -9,11 +9,15 @@ int main() {
     int a = 1;
     int b = 2;
 
-    printf("A = %d\nB = %d\n", a, b);
+    // printf("A = %d\nB = %d\n", a, b);
 
     int res = a & b;
 
     printf("%d\n", res);
+
+    // То есть результат 1 получается только если оба бита равны 1.
+
+    
 
     return 0;
 }
