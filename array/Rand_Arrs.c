69a0d418740e889e703c033f803a992ef2cb96ee
RusProgger <ruslantimka92@gmail.com>
2026-08-20 15:39:16 +0300

    Add library time

--- array/Rand_Arrs.c
+++ array/Rand_Arrs.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
 
     int arrsRandom[5];
+    srand(time(NULL));
 
     for(int i = 0; i < sizeof(arrsRandom) / sizeof(arrsRandom[0]); i++) {
         arrsRandom[i] = rand() % 50 + 1;
     }
 
-    
+     for(int i = 0; i < sizeof(arrsRandom) / sizeof(arrsRandom[0]); i++) {
+        printf("Array random: %d\n", arrsRandom[i]);
+    }
 
     return 0;
 }
