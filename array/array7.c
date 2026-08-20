e8243a59169abb2e09e3b43096494b8b9e7adaa3
RusProgger <ruslantimka92@gmail.com>
2026-08-20 15:12:33 +0300

    Add array loop

--- array/array7.c
+++ array/array7.c
@@ -2,6 +2,12 @@
 
 int main() {
 
+    unsigned int arrs[5] = {1, 5, 10, 2, 3};
+
+    for(int i = 0; i < sizeof(arrs) / sizeof(arrs[0]); i++) {
+        printf("Array %d\n", arrs[i]);
+    }
+
     
 
     getchar();
